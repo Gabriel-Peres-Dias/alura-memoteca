b78d7f75fe48fc5bd5797e49bb2ca012bfcd7727
Gabriel Peres <gabriel.suporte123@gmail.com>
2023-05-08 22:38:37 -0300

    adicionando o módulo do  forms + criando om componente de ciração de pensamentos + criando o formulário no template + conhencendo os binds que podem ser: () -> eventsBind do template(html) para componente(ts), [] do componente(ts) para tempalte(html) e o two-way-dataBinding [()] que são em ambas as direções.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo2'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public criarPensamento(): void {
+    alert('criquei crias');
+  }
+
+  public cancelar(): void {
+    alert('cancelado');
   }
 
 }
